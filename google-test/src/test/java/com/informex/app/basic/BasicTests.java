ce51a1f9c1161d3dfa802388165875ba7253edfe
Saul Osorio <saul.osorio@solera.com>
2023-03-02 15:50:09 +0100

    created and parametrized setup method to work both with firefox and chrome

--- google-test/src/test/java/com/informex/app/basic/BasicTests.java
+++ google-test/src/test/java/com/informex/app/basic/BasicTests.java
@@ -1,17 +1,16 @@
 package com.informex.app.basic;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import com.informex.app.utils.Utils;
 import com.informex.app.behavior.mainpage.MainPage;
 
+	
 public class BasicTests {
-
+	
 	@Test
 	public void loginTest() {
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = Utils.setup("firefox");
 		MainPage.goToMainPage(driver);
 		Utils.sleep(1500);
 		MainPage.acceptCookies(driver);
@@ -20,4 +19,6 @@ public class BasicTests {
 		Utils.sleep(1500);
 		driver.quit();
 	}
+	
 }
+
