93df48974e405a7b1f91f7147dd4308d6605989c
Saul Osorio <saul.osorio@solera.com>
2023-03-03 10:02:07 +0100

    Removed hard coded values in MainPage class methods.

--- google-test/src/test/java/com/informex/app/behavior/mainpage/MainPage.java
+++ google-test/src/test/java/com/informex/app/behavior/mainpage/MainPage.java
@@ -1,23 +1,31 @@
 package com.informex.app.behavior.mainpage;
 
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.informex.app.utils.TestProperties;
+
 public class MainPage {
+	private static Properties properties = TestProperties.getProperties();
 	
 	public static void acceptCookies(WebDriver driver) {
-		WebElement cookieButton = driver.findElement(By.id("L2AGLb"));
+		String id = properties.getProperty("acceptcookiesbuttonid");
+		WebElement cookieButton = driver.findElement(By.id(id));
 		cookieButton.click();
 	}
 	
 	public static void enterASearch(WebDriver driver, String search) {
-		WebElement searchInput = driver.findElement(By.xpath("//input[@name = 'q']"));
+		String xpath = properties.getProperty("searchInputXpath");
+		WebElement searchInput = driver.findElement(By.xpath(xpath));
 		searchInput.sendKeys(search + "\n");
 	}
 	
 	public static void goToMainPage(WebDriver driver) {
-		String url = "https://www.google.com/";
+		Properties properties = TestProperties.getProperties();
+		String url = properties.getProperty("googleurl");
 		driver.get(url);
 		driver.manage().window().maximize();
 	}
