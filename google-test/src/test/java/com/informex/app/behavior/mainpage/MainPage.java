8dd61c3e87eae91e749a7420b80b2de3ff4540ac
Saul Osorio <saul.osorio@solera.com>
2023-03-03 10:15:01 +0100

    Removed implicit waits from the test class and added explicit waits into the behavior methods

--- google-test/src/test/java/com/informex/app/behavior/mainpage/MainPage.java
+++ google-test/src/test/java/com/informex/app/behavior/mainpage/MainPage.java
@@ -1,10 +1,13 @@
 package com.informex.app.behavior.mainpage;
 
+import java.time.Duration;
 import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.informex.app.utils.TestProperties;
 
@@ -13,12 +16,14 @@ public class MainPage {
 	
 	public static void acceptCookies(WebDriver driver) {
 		String id = properties.getProperty("acceptcookiesbuttonid");
+		new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.elementToBeClickable(By.id(id)));
 		WebElement cookieButton = driver.findElement(By.id(id));
 		cookieButton.click();
 	}
 	
 	public static void enterASearch(WebDriver driver, String search) {
 		String xpath = properties.getProperty("searchInputXpath");
+		new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(xpath)));
 		WebElement searchInput = driver.findElement(By.xpath(xpath));
 		searchInput.sendKeys(search + "\n");
 	}
